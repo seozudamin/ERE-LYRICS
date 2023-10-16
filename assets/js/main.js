2608e43dbd9aece3e9a714d52f9f9e564a2045cb
SEOZU <100259508+seozudamin@users.noreply.github.com>
2023-10-17 07:43:32 +0800

    ERE LYRICS

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+
+AOS.init({
+
+  offset: 120, 
+  delay: 0, 
+  duration: 700, 
+  easing: 'ease', 
+  once: false, 
+  mirror: false, 
+  anchorPlacement: 'top-bottom',
+
+});
+
+var typed = new Typed(".auto-type", {
+  strings: [ "10pm na  pala shet!!!!!!!!", "tulonggggggg", "guuuurrwahhhhhhhhhhh", "Tatlong bilyon, ikaw lang ang aking gusto", "Pasensiya na kung ngayon ako'y 'di para sa 'yo", "Tayo ay papunta na sa 'ting bagong yugto", "Yokong mabuhay sa isang mundong walang tayo"],
+  typeSpeed: 102,
+  backSpeed: 1,
+  loop: true
+})
+
+
